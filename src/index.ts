c4dba5f02b30ecb662bdc1f5eb9d8f3025b82bc2
TusharSachdeva29 <161271923+TusharSachdeva29@users.noreply.github.com>
2025-01-16 22:38:23 +0530

    bug resolve in express code

--- src/index.ts
+++ src/index.ts
@@ -1,46 +1,16 @@
-// import WebSocket, { WebSocketServer } from 'ws';
-// import http from 'http';
-// //first reqesust is an http request only when server gets http its giets upgreaded to websocket
+import WebSocket, { WebSocketServer } from 'ws';
+import http from 'http';
+//first reqesust is an http request only when server gets http its giets upgreaded to websocket
 
-// // this is how we write server natively in node js without use of express
-
-
-// const server = http.createServer(function(request: any, response: any) {
-//     console.log((new Date()) + ' Received request for ' + request.url);
-//     response.end("hi there");
-// });
-
-// const wss = new WebSocketServer({ server });
-
-// wss.on('connection', function connection(ws) {
-//   ws.on('error', console.error);
-
-//   ws.on('message', function message(data, isBinary) {
-//     wss.clients.forEach(function each(client) {
-//       if (client.readyState === WebSocket.OPEN) {
-//         client.send(data, { binary: isBinary });
-//       }
-//     });
-//   });
-
-//   ws.send('Hello! Message From Server!!');
-// });
-
-// server.listen(8080, function() {
-//     console.log((new Date()) + ' Server is listening on port 8080');
-// });
+// this is how we write server natively in node js without use of express
 
 
+const server = http.createServer(function(request: any, response: any) {
+    console.log((new Date()) + ' Received request for ' + request.url);
+    response.end("hi there");
+});
 
-// with use of express
-
-import express from 'express'
-import { WebSocketServer } from 'ws'
-
-const app = express()
-const httpServer = app.listen(8080)
-
-const wss = new WebSocketServer({ server: httpServer });
+const wss = new WebSocketServer({ server });
 
 wss.on('connection', function connection(ws) {
   ws.on('error', console.error);
@@ -55,3 +25,33 @@ wss.on('connection', function connection(ws) {
 
   ws.send('Hello! Message From Server!!');
 });
+
+server.listen(8080, function() {
+    console.log((new Date()) + ' Server is listening on port 8080');
+});
+
+
+
+// with use of express , express give clean code to handkle 
+
+// import express from 'express'
+// import { WebSocketServer , WebSocket } from 'ws'
+
+// const app = express()
+// const httpServer = app.listen(8080)
+
+// const wss = new WebSocketServer({ server: httpServer });
+
+// wss.on('connection', function connection(ws) {
+//   ws.on('error', console.error);
+
+//   ws.on('message', function message(data, isBinary) {
+//     wss.clients.forEach(function each(client) {
+//       if (client.readyState === WebSocket.OPEN) {
+//         client.send(data, { binary: isBinary });
+//       }
+//     });
+//   });
+
+//   ws.send('Hello! Message From Server!!');
+// });
